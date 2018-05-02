6cdfa04db62cb478df55eea8ea6a573128856291
dacharat <dacharat.p@ku.th>
2018-05-02 19:35:06 +0700

    :snake: Piece can move

--- src/gameUI/PLayerMovePiece.java
+++ src/gameUI/PLayerMovePiece.java
@@ -18,36 +18,40 @@ public class PLayerMovePiece extends Task<Integer> {
 	}
 
 	public void moveUp() throws InterruptedException {
-		for (int i = 0; i < 60; i++) {
-			playerImage.setTranslateY(playerImage.getTranslateY() + 1);
-			Thread.sleep(10);
-		}
-	}
-
-	public void moveLeft() throws InterruptedException {
-		for (int i = 0; i < 60; i++) {
-			playerImage.setTranslateX(playerImage.getTranslateX() + 1);
-			Thread.sleep(10);
-		}
+		// for (int i = 0; i < 60; i++) {
+		playerImage.setLayoutY(playerImage.getLayoutY() - 60);
+		// playerImage.setTranslateX(playerImage.getTranslateX());
+		// playerImage.setTranslateY(playerImage.getTranslateY() + 1);
+		// }
 	}
 
 	public void moveRight() throws InterruptedException {
-		for (int i = 0; i < 60; i++) {
-			playerImage.setTranslateX(playerImage.getTranslateX() - 1);
-			Thread.sleep(10);
-		}
+		// for (int i = 0; i < 60; i++) {
+		playerImage.setLayoutX(playerImage.getLayoutX() + 60);
+		// playerImage.setTranslateX(playerImage.getTranslateX() + 1);
+		// playerImage.setTranslateY(playerImage.getTranslateY());
+		// }
+	}
+
+	public void moveLeft() throws InterruptedException {
+		// for (int i = 0; i < 60; i++) {
+		playerImage.setLayoutX(playerImage.getLayoutX() - 60);
+		// playerImage.setTranslateX(playerImage.getTranslateX() - 1);
+		// playerImage.setTranslateY(playerImage.getTranslateY());
+		// }
 	}
 
 	@Override
 	protected Integer call() throws Exception {
-		int position = game.getPlayerPosition(player) + 1;
-		System.out.println(game.currentPlayerName() + " on:" + position);
-		if (position % 20 < 10) {
-			moveLeft();
-		} else if (position % 20 > 10) {
-			moveRight();
-		} else if (position % 20 == 10 || game.currentPlayerPosition() % 20 == 0) {
+		int position = game.getPlayerPosition(player) ;
+		System.out.println("Player on: " + position);
+		if (position % 20 == 0 || position % 20 == 10) {
+			System.out.println("Move Up!!");
 			moveUp();
+		} else if (position % 20 <= 10) {
+			moveRight();
+		} else if (position % 20 >= 10) {
+			moveLeft();
 		}
 		return null;
 	}
