caf8331819bb0958da0db4771b62ee944ca9a286
dacharat <dacharat.p@ku.th>
2018-05-03 08:55:20 +0700

    :snake: Merge file

--- src/gameUI/PLayerMovePiece.java
+++ src/gameUI/PLayerMovePiece.java
@@ -17,29 +17,6 @@ public class PLayerMovePiece extends Task<Integer> {
 		this.playerImage = playerImage;
 	}
 
-<<<<<<< HEAD
-	public void moveUp() {
-		playerImage.setTranslateY(playerImage.getTranslateY() - 60);
-	}
-
-	public void moveLeft() {
-		playerImage.setTranslateX(playerImage.getTranslateX() - 60);
-	}
-
-	public void moveRight() {
-		playerImage.setTranslateX(playerImage.getTranslateX() + 60);
-	}
-
-	@Override
-	protected Integer call() throws InterruptedException {
-		int position = game.getPlayerPosition(player) + 1;
-		System.out.println(game.currentPlayerName() + " on:" + position);
-		if (position % 20 < 10) {
-			moveRight();
-		} else if (position % 20 > 10) {
-			moveLeft();
-		} else if (position % 20 == 10 || game.currentPlayerPosition() % 20 == 0) {
-=======
 	public void moveUp() throws InterruptedException {
 		// for (int i = 0; i < 60; i++) {
 		playerImage.setLayoutY(playerImage.getLayoutY() - 60);
@@ -70,14 +47,13 @@ public class PLayerMovePiece extends Task<Integer> {
 		System.out.println("Player on: " + position);
 		if (position % 20 == 0 || position % 20 == 10) {
 			System.out.println("Move Up!!");
->>>>>>> 8c03301f289e538138413d7a1903704bf0689741
 			moveUp();
 		} else if (position % 20 <= 10) {
 			moveRight();
 		} else if (position % 20 >= 10) {
 			moveLeft();
 		}
-		Thread.sleep(20);
+//		Thread.sleep(20);
 		return null;
 	}
 
